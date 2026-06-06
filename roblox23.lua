e9fea32ab8cba8f49d7580312f276f969cb58513
larpbaseinate-stack <larpbaseinate@gmail.com>
2026-06-06 02:29:21 -0400

    Add welcome message for Admin A3hv joining server

---
+++ roblox23.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage", {
+    Text = "Server: Admin A3hv has joined the server",
+    Color = Color3.fromRGB(255, 255, 255),
+    Font = Enum.Font.GothamBold,
+    FontSize = Enum.FontSize.Size18
+})
