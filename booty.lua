d57cdc4c064c9c4937ac92e7d02fd3a87ce3e59d
larpbaseinate-stack <larpbaseinate@gmail.com>
2026-06-06 02:28:01 -0400

    Create booty.lua

---
+++ booty.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage", {
+    Text = "Server: welcome",
+    Color = Color3.fromRGB(255, 255, 255),
+    Font = Enum.Font.GothamBold,
+    FontSize = Enum.FontSize.Size18
+})
