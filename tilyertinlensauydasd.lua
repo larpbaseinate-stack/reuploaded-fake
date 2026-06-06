5eaf152a00a9e98f2a46078c13028e11581f50b5
larpbaseinate-stack <larpbaseinate@gmail.com>
2026-06-06 02:44:38 -0400

    Add welcome message for Admin A3hv in chat

---
+++ tilyertinlensauydasd.lua
@@ -0,0 +1,4 @@
+local TextChatService = game:GetService("TextChatService")
+
+local channel = TextChatService.TextChannels:WaitForChild("RBXGeneral")
+channel:DisplaySystemMessage("Server: Admin A3hv has joined the Server")
